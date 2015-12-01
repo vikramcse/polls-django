a7b7b24ca18555857010a962552c406278b22ea8
Vikram Jadhav <vikramcse.10@gmail.com>
2015-12-01 14:00:44 +0000

    Tutorial no 4 completed: basic views and changed urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,18 @@ from django.conf.urls import url
 
 from . import views
 
+# When Django finds a regular expression match, Django calls the specified view function, 
+# with an HttpRequest object as the first argument and any captured values from the 
+# regular expression as other arguments.
+# In this case the index function is called in the views.py
+
 urlpatterns = [
+	# ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
